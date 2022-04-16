416686f4b5368f28729773dac1a175d8f32acc19
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-04-16 17:04:59 +0300

    feat: add preview section

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,16 +1,16 @@
 export function isWebp() {
     function testWebP(callback) {
 
-        var webP = new Image();
+        let webP = new Image();
         webP.onload = webP.onerror = function () {
-            callback(webP.height == 2);
+            callback(webP.height === 2);
         };
         webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
     }
 
     testWebP(function (support) {
 
-        if (support == true) {
+        if (support === true) {
             document.querySelector('body').classList.add('webp');
         }else{
             document.querySelector('body').classList.add('no-webp');
