0f35ddb55a4bd437aa2ba343ff53f30fd72e8f6f
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-04-25 11:41:07 +0300

    feat: add style for burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
 
-flsFunctions.isWebp()
+let menuIcon = document.querySelector('.icon-menu');
+
+flsFunctions.isWebp();
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('_active');
+});
+
