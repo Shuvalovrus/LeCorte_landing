be87d16d81b6ddf541016431e4446766eb66c9d0
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-04-25 12:06:17 +0300

    feat: add adaptive burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import * as flsFunctions from "./modules/functions.js";
 
 let menuIcon = document.querySelector('.icon-menu');
+let header = document.querySelector('.header');
 
 flsFunctions.isWebp();
 
 menuIcon.addEventListener('click', () => {
+    header.classList.toggle('_active');
     menuIcon.classList.toggle('_active');
 });
 
