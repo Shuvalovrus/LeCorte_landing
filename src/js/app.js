5938d4dba23d3cf805d4c18b1acb241c29a50b26
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-04-25 13:07:22 +0300

    fix; fixed footer style, and stop croll on open menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,16 @@ import * as flsFunctions from "./modules/functions.js";
 
 let menuIcon = document.querySelector('.icon-menu');
 let header = document.querySelector('.header');
+let body = document.querySelector('body');
+
+
 
 flsFunctions.isWebp();
 
 menuIcon.addEventListener('click', () => {
+    body.classList.toggle('no-scroll')
     header.classList.toggle('_active');
     menuIcon.classList.toggle('_active');
+    menuIcon.preventDefault()
 });
 
