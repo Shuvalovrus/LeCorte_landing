95b2bc8bfe3a495485d2a556a57956f94088614f
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-05-07 21:40:45 +0300

    Update 2022-05-07T18:40:38.876Z

--- gulp/tasks/public.js
+++
@@ -1,6 +0,0 @@
-import ghPages from 'gulp-gh-pages';
-
-export const gitPush = () => {
-    return app.gulp.src('./dist/**/*')
-        .pipe(ghPages());
-}
--- gulp/tasks/reset.js
+++
@@ -1,6 +0,0 @@
-// Задача отчистки папок
-import del from "del";
-export const reset = () => {
-    return del(app.path.clean)
-}
-
