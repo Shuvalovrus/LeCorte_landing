cd30dbbcfc625a496309602fcf7a97c519968a97
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-05-07 21:12:37 +0300

    feat:add gift dep

--- gulp/tasks/public.js
+++ gulp/tasks/public.js
@@ -1,6 +1,6 @@
 import ghPages from 'gulp-gh-pages';
 
 export const gitPush = () => {
-    return app.gulp.src('./dist/**/*')
+    return app.gulp.src(app.path.build.pages)
         .pipe(ghPages());
 }
